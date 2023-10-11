0847e555ef4a69e18ac2d785cc6f10e0e922d922
PatelPanth <146762440+PatelPanth@users.noreply.github.com>
2023-10-11 18:38:00 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+  background ("white")
+}
+
+function draw() 
+{
+
+  if (keyIsDown(RIGHT_ARROW)) {
+ background ("red");
+  }
+if (keyIsDown(LEFT_ARROW)) {
+background ("blue");
+}
+if (keyIsDown(DOWN_ARROW)) {
+background ("green");
+}
+if (keyIsDown(UP_ARROW)) {
+ background ("yellow")
+}
+}
